28a9cebbe1018a32eaae3eb331b51cf10d7c4d61
Valeria2262 <rams2015@i.ua>
2024-01-25 23:40:48 +0200

    Add HW 8

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-
-const categories = document.querySelectorAll(".item");
-
-console.log(`Number of categories: ${categories.length}`);
-categories.forEach((elem) => {
-  console.log(`Category: ${elem.firstElementChild.textContent}`);
-  console.log(`Elements: ${elem.lastElementChild.children.length}`);
-});
